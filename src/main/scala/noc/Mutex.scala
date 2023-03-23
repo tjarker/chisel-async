57a38848c716c36e59a201127cf9f7b178f29ab8
katerina-pravdova <45755904+KateTruth@users.noreply.github.com>
2023-03-23 18:58:24 +0100

    mutex

---
+++ src/main/scala/noc/Mutex.scala
@@ -0,0 +1,43 @@
+package noc
+import async.Handshake
+import chisel3._
+import helpers.Hardware.ToggleReg
+import helpers.TreeReducer
+import noc.Channel.OutboundChannel
+import chisel3.util.HasBlackBoxInline
+class MutexBox extends BlackBox with HasBlackBoxInline {
+  val io = IO(new Bundle {
+    val req1= Input(UInt(1.W))
+    val req2= Input(UInt(1.W))
+    val grant1 = Output(UInt(1.W))
+    val grant2 = Output(UInt(1.W))
+  })
+  setInline("module MutexBox.v",
+    s"""module MutexBox(
+       |  input req1,
+       |  input req2,
+       |  output grant1,
+       |  output grant2
+       |);
+       |
+       |wire o1 = ~(req1 & o2);
+       |wire o2 = ~(req2 & o1);
+       |assign{grant1} = ~o1 & o2;
+       |assign{grant2} = ~o2 & o1;
+       |
+       |endmodule""".stripMargin)
+}
+
+class Mutex extends Module {
+    val io = IO(new Bundle {
+    val req1 = Input(UInt(1.W))
+    val req2 = Input(UInt(1.W))
+    val grant1 = Output(UInt(1.W))
+    val grant2 = Output(UInt(1.W))
+  })
+    val m = Module(new MutexBox())
+    m.io.req1 := io.req1
+    m.io.req2 := io.req2
+    io.grant1 := m.io.grant1
+    io.grant2 := m.io.grant2
+}
