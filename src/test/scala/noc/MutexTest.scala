92fa84b90a51bade5505a6ab1faf16f0bb944e63
katerina-pravdova <45755904+KateTruth@users.noreply.github.com>
2023-03-26 17:13:42 +0200

    update mutextest with new io

--- src/test/scala/noc/MutexTest.scala
+++ src/test/scala/noc/MutexTest.scala
@@ -9,17 +9,24 @@ import noc.Direction.{East, NorthWest}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class MutexTest extends AnyFlatSpec with
-  ChiselScalatestTester {
+  ChiselScalatestTester{
 
   "Mutex" should "arbitrate outputs" in {
 
-    test(new Mutex) { dut =>
-        dut.io.req1.poke("b1".U)
-        dut.io.req2.poke("b0".U)
+    test(new Mutex).withAnnotations(Seq(
+      IcarusBackendAnnotation,
+      WriteVcdAnnotation
+    )) { dut =>
+        dut.io.req(0).poke("b1".U)
+        dut.io.req(1).poke("b0".U)
         dut.clock.step()
-        dut.io.grant1.expect("b1".U)
-
-
+        dut.io.grant(0).expect("b1".U)
+        dut.io.grant(1).expect("b0".U)
+        dut.io.req(0).poke("b0".U)
+        dut.io.req(1).poke("b1".U)
+        dut.clock.step()
+        dut.io.grant(0).expect("b0".U)
+        dut.io.grant(1).expect("b1".U)
     }
   }
 
