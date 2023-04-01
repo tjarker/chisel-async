aed41dfd6feb22a500434029aceb3f26bdfa2e0a
katerina-pravdova <45755904+KateTruth@users.noreply.github.com>
2023-04-01 10:33:59 +0200

    enhancements & simulation delay

--- src/main/scala/noc/ArbMerge.scala
+++ src/main/scala/noc/ArbMerge.scala
@@ -2,8 +2,10 @@ package noc
 import async.Handshake
 import chisel3._
 import helpers.Hardware.ToggleReg
+import helpers.Hardware.SetRegWithValue
 import helpers.TreeReducer
 import noc.Channel.OutboundChannel
+import async.blocks.SimulationDelay.SimulationDelayer
 //package async.blocks
 import chisel3.util.HasBlackBoxInline
 import helpers.Types.{Coordinate, GridBuilder}
@@ -15,33 +17,26 @@ class Merge[P <: Data]()(implicit p:NocParameters[P]) extends Module {
   })
   //new request detected
   val sela = io.in(0).ack  ^ io.in(0).req
-  val oneof = sela | io.in(1).ack ^ io.in(1).req
-
+  val clickOut = sela | (io.in(1).ack ^ io.in(1).req)
+    .addSimulationDelay(1)
   //send request out when new request processed
-  withClockAndReset(oneof.asClock, reset.asAsyncReset) {
+  withClockAndReset(clickOut.asClock, reset.asAsyncReset) {
     io.out.req := ToggleReg(0.B)
     io.out.data := Mux(sela, io.in(0).data, io.in(1).data)
   }
-
   //ack received after request was sent
-  val done = !(io.out.ack ^ io.out.req)
+  val clickIn = !(io.out.ack ^ io.out.req)
+    .addSimulationDelay(1)
   //create ack nodes
-  val ack2Node = Wire(UInt(1.W))
-  val ack1Node = Wire(UInt(1.W))
-  io.in(0).ack := ack1Node
-  io.in(1).ack := ack2Node
+  val ackNode = Wire(Vec(2, UInt(1.W)))
   //send ack when received ack
-  withClockAndReset(done.asClock, reset.asAsyncReset) {
-    //TODO:simplify into one
-    val ack1Reg = RegInit(0.B)
-    ack1Reg := io.in(0).req
-    ack1Node := ack1Reg
-
-    val ack2Reg = RegInit(0.B)
-    ack2Reg := io.in(1).req
-    ack2Node := ack2Reg
+  withClockAndReset(clickIn.asClock, reset.asAsyncReset) {
+    ackNode.zip(io.in).map { case (ackout, in) => {
+      in.ack := ackout
+      ackout := SetRegWithValue(in.req, 1.B)
+      }
+    }
   }
-
 }
 
 class Arbiter[P <: Data]()(implicit p: NocParameters[P]) extends Module {
@@ -54,63 +49,33 @@ class Arbiter[P <: Data]()(implicit p: NocParameters[P]) extends Module {
   val m = Module(new Mutex())
 
   val grantReleased = Wire(Vec(2,UInt()))
-  val reqChange = Wire(Vec(2, UInt()))
+  val clickOut = Wire(Vec(2, UInt()))
   val prevState = Wire(Vec(2, UInt()))
-  val rout = Wire(Vec(2, UInt(2.W)))
+  val clickIn = Wire(Vec(2, UInt()))
 
-  //TODO : simplify the bottom code into one (maybe with for loop?) - merge upper and lower path - and add comments
+    for (i <- 0 until 2) {
+      //click if mutex grants
+      clickIn(i) := m.io.grant(i)
+        .addSimulationDelay(2)
+      withClockAndReset(clickIn(i)(0).asClock, reset.asAsyncReset) {
+        //send feedback about request
+        grantReleased(i) := ToggleReg(0.B)
+        //send request to the output
+        io.out(i).req := ToggleReg(0.B)
+      }
+      //is there a new request on the input?
+      clickOut(i) := (!grantReleased(i) & io.in(i).req & !prevState(i)) | (grantReleased(i) & !io.in(i).req & prevState(i))
 
-//    for (i <- 0 until 1) {
-//      rout(i) := m.io.grant(i)
-//      withClockAndReset(rout(i)(0).asClock, reset.asAsyncReset) {
-//        grantReleased(i) := ToggleReg(0.B)
-//        io.out(i).req := ToggleReg(0.B)
-//      }
-//
-//      reqChange(i) := (!grantReleased(i) & io.in(i).req & !prevState(i)) | (grantReleased(i) & !io.in(i).req & prevState(i))
-//
-//      withClockAndReset(reqChange(i)(0).asClock, reset.asAsyncReset) {
-//        prevState(i) := ToggleReg(0.B)
-//      }
-//      m.io.req(i) := io.out(i).ack ^ prevState(i)
-//      io.in(i).ack := io.out(i).ack
-//      io.out(i).data := io.in(i).data
-//    }
-  //upper path
-  //request if mutex grants
-  rout(0) := m.io.grant(0)
-  withClockAndReset(rout(0)(0).asClock, reset.asAsyncReset) {
-    //send feedback about request
-    grantReleased(0) :=  ToggleReg(0.B)
-    //send request to output
-    io.out(0).req := ToggleReg(0.B)
-  }
-  //is there a new request on the input?
-  reqChange(0) := (!grantReleased(0) & io.in(0).req & !prevState(0)) | (grantReleased(0) & !io.in(0).req & prevState(0))
+      withClockAndReset(clickOut(i)(0).asClock, reset.asAsyncReset) {
+        //save current state for future request check
+        prevState(i) := ToggleReg(0.B)
+      }
+      //request to mutex if input request changed
+      m.io.req(i) := io.out(i).ack ^ prevState(i)
+      io.in(i).ack := io.out(i).ack
+      io.out(i).data := io.in(i).data
+    }
 
-  withClockAndReset(reqChange(0)(0).asClock, reset.asAsyncReset) {
-    //safe current state for future request check
-    prevState(0) := ToggleReg(0.B)
-  }
-  //request to mutex if input request changed
-  m.io.req(0) := io.out(0).ack ^ prevState(0)
-  io.in(0).ack := io.out(0).ack
-  io.out(0).data := io.in(0).data
-
-  //lower path - TODO - merge with upper path
-  rout(1) := m.io.grant(1)
-  withClockAndReset(rout(1)(0).asClock, reset.asAsyncReset) {
-    grantReleased(1) := ToggleReg(0.B)
-    io.out(1).req := ToggleReg(0.B)
-  }
-  reqChange(1) := (!grantReleased(1) & io.in(1).req & !prevState(1)) | (grantReleased(1) & !io.in(1).req & prevState(1))
-
-  withClockAndReset(reqChange(1)(0).asClock, reset.asAsyncReset) {
-    prevState(1) := ToggleReg(0.B)
-  }
-  m.io.req(1) := io.out(1).ack ^ prevState(1)
-  io.in(1).ack := io.out(1).ack
-  io.out(1).data := io.in(1).data
 }
 
 
